cd1461524278f22d53f8220cdd83a10f2706d508
goghvanmr <eagle.liyi@gmail.com>
2013-02-19 18:24:13 +0800

    player now can execute roll command

--- src/RollCommand.java
+++ src/RollCommand.java
@@ -1,7 +1,3 @@
-import sun.jvm.hotspot.memory.DictionaryEntry;
-
-import java.util.Random;
-
 public class RollCommand extends Command {
     private Player player;
 
@@ -11,9 +7,6 @@ public class RollCommand extends Command {
 
     @Override
     public void execute() {
-        Random random = new Random();
-
-        int steps = random.nextInt(6);
         player.forward(Dice.roll());
     }
 }
